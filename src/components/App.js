96a8ddbf8f9243afab29a90299ee1d6fb8592120
Emilijan Koteski <emilijankoteski@pm.me>
2022-07-30 12:56:02 +0200

    Init commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+/**
+ *
+ * Todo List App
+ * 
+ * This is a simple react application.
+ *  
+ * @author Emilijan Koteski <emilijan.koteski@pm.me>
+ * 
+ */
+
+function App() {
+  return (
+    <div className="display-6">
+      Project init.
+    </div>
+  );
+}
+
+export default App;
